18f3050571a320ec2ab169a7f41ccc3472bfed09
subhashprasad2001@gmail.com <96110162+subhashdippu@users.noreply.github.com>
2024-04-11 17:45:39 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    stirng: ["Software Developer", "Backend Developer, Artifcial Intelligence Developer and Machine Learnig Engineer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    BackDelay: 1000,
+    loop: true
+})
